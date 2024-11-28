3aeaa104e13ccfcae3cf7ecb7ddbd6b5be5921cc
Adam Steppe <aws69@gitlab.com>
2024-11-28 13:45:16 -0500

    added more vector math functions

--- vector.c
+++ vector.c
@@ -9,3 +9,39 @@ Vector4 add(Vector4 a, Vector4 b){
 	};
 	return result;
 }
+
+Vecotr sub(Vecotr a, Vector b){
+	Vector4 result = {
+		a.x - b.x,
+		a.y - b.y,
+		a.z - b.z,
+		a.w - b.w
+	};
+	return result;
+}
+
+Vector4 mult(float a, Vector4 b){
+	Vector4 result = {
+		a * b.x,
+		a * b.y,
+		a * b.z,
+		a * b.w
+	};
+	return result;
+}
+
+float dot(Vector4 a, Vector4 b){
+	float result = (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+	return result;
+}
+
+Vector4 cross(Vector4 a, Vector4 b){
+	Vector4 result = {
+		(a.y * b.z) - (a.z * b.y),
+		(a.z * b.x) - (a.x * b.z),
+		(a.x * b.y) - (a.y * b.x),
+		0
+	};
+	return result;
+}
+
--- vector.h
+++ vector.h
@@ -7,3 +7,7 @@ typedef struct {
 } Vector4;
 
 Vector4 add(Vector4, Vector4);
+Vector4 sub(Vector4, Vecotr4);
+Vector4 mult(float, Vector4);
+float dot(Vector4, Vector4);
+Vector4 cross(Vector4, Vector4);
